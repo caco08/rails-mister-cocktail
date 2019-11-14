bc4303babc13f4b49031b1f239c156f1ed66d931
carlos silva <carlosantonio.dsilva@gmail.com>
2019-11-14 16:53:37 -0300

    models

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,16 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   movies = Movie.cr
+require 'open-uri'
+
+Ingredient.destroy_all
+
+Cocktail.destroy_all
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredients = JSON.parse(open(url).read)
+ingredients["drinks"].each do |ingredient|
+  i = Ingredient.create(name: ingredient["strIngredient1"])
+  puts "create #{i.name}"
+end
